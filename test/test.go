eb9cfe0ecb78800e1ead55ef94b90a51e33543ea
Aladik <aladik95@gmail.com>
2022-10-05 19:36:31 +0600

    inter done

--- test/test.go
+++ test/test.go
@@ -1,11 +1,28 @@
 package main
 
-import (
-	"fmt"
-	"piscine"
-)
-
 func main() {
-	l := piscine.StrLen("Hello World!")
-	fmt.Println(l)
+	// inter
+	// args := os.Args
+	// s := ""
+	// have := false
+	// if len(args) == 3 {
+	// 	for _, v := range args[1] {
+	// 		have = false
+	// 		for _, k := range args[2] {
+	// 			if v == k {
+	// 				for _, f := range s {
+	// 					if k == f {
+	// 						have = true
+	// 						break
+	// 					}
+	// 				}
+	// 				if !have {
+	// 					s += string(k)
+	// 				}
+	// 				break
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// fmt.Println(s)
 }
